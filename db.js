b434bcdad25c027045f137c5b1b844ce3e06ea92
jignesh patel <jignesh3094@gmail.com>
2024-04-10 12:57:36 +0530

    changes node js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const {MongoClient,ObjectId} = require('mongodb');
-const uri = "mongodb://localhost:27017";
+const uri = "mongodb+srv://admin:admin@cluster0.ny4riyc.mongodb.net/";
 const client = new MongoClient(uri);
 const db = client.db("newsapp");
 module.exports = {db,ObjectId};
